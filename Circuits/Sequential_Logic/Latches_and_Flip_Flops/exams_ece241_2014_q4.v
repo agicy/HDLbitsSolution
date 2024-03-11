8a96bf520136430196abd903b3dc2884d5171387
agicy <2359800311@qq.com>
2024-03-11 19:45:05 +0800

    add solution for problem exams_ece241_2014_q4

---
+++ Circuits/Sequential_Logic/Latches_and_Flip_Flops/exams_ece241_2014_q4.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input x,
+    output z
+);
+    wire d1, d2, d3, q1, q2, q3;
+    assign d1 = q1 ^ x;
+    assign d2 = x & ~ q2;
+    assign d3 = x | ~ q3;
+    always @(posedge clk) begin
+        q1 = d1;
+        q2 = d2;
+        q3 = d3;
+    end
+    assign z = ~ (q1 | q2 | q3);
+endmodule
